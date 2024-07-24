0152aefe437b6c211abf9a7289278a4f03cf0721
Jan Holda <janholda@seznam.cz>
2024-07-24 21:01:17 +0200

    Added bootstrap + font awesome

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { platformBrowser } from '@angular/platform-browser';
 import { AppModule } from './app/app.module';
 
-
+platformBrowser().bootstrapModule(AppModule).catch((err) => console.error(err));
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
