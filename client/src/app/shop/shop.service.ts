c2cf92ca703a56f202e3b084d9b42a2b504330fb
Jan Holda <janholda@seznam.cz>
2024-07-25 19:48:01 +0200

    Angular Routing added

--- client/src/app/shop/shop.service.ts
+++ client/src/app/shop/shop.service.ts
@@ -13,6 +13,10 @@ export class ShopService {
   baseUrl = 'https://localhost:5001/api/';
 
   constructor(private http: HttpClient) { }
+  
+  getProduct(id: number){
+    return this.http.get<Product>(this.baseUrl + 'products/' + id)
+  }
 
   getProducts(shopParams: ShopParams){
     let params = new HttpParams();
